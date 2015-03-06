85558d1836b4dd48e1f6217dab8195846ba22e65
hammas95 <hammas95@outlook.com>
2015-03-06 11:28:43 +0100

    Update Stop.java

--- src/lab5/simulator/Stop.java
+++ src/lab5/simulator/Stop.java
@@ -1,7 +1,26 @@
 package lab5.simulator;
 
-public class Stop {
-	public void run(){
-		
+/**
+ *Stop that stops the current simulation that it is being processed in. 
+ *
+ */
+public class Stop extends Event {
+		   public Stop(double time) {
+			   super("Stop", time);
+		    }
+		    @Override
+		    public void execute(Simulator sim) {
+		        super.execute(sim);
+		        sim.stop();
+		    }
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				
+			}
+			@Override
+			public String toString() {
+				// TODO Auto-generated method stub
+				return null;
 	}
 }
