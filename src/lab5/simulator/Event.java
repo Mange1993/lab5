818392993013dd701572ea20bf9f4516a84c06a8
hammas95 <hammas95@outlook.com>
2015-03-06 11:29:30 +0100

    Update Event.java

--- src/lab5/simulator/Event.java
+++ src/lab5/simulator/Event.java
@@ -1,11 +1,25 @@
 package lab5.simulator;
 
+/**
+ * An abstract named and timed Event representation. References the simulator.
+ */
+
 public abstract class Event {
-	
-	
 	
 	public abstract void run();
 		
 	public abstract String toString();
+	
+	protected String _type;
+	protected double _time;
+    protected Simulator _simulator;
+    
+    public Event(String type, double time) {
+        _type = type;
+        _time = time;
+    }
 
+	public void execute(Simulator sim) {
+        _simulator = sim;
+    }
 }
