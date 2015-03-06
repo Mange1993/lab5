53471c272dc6e3688e00023d78354cd5064d67e2
hammas95 <hammas95@outlook.com>
2015-03-06 11:28:17 +0100

    Update Start.java

--- src/lab5/simulator/Start.java
+++ src/lab5/simulator/Start.java
@@ -1,7 +1,28 @@
 package lab5.simulator;
 
-public class Start {
-	public run();
-		
+/**
+ * 
+ * Simulator "startEvent"
+ * Should tell the simulator to start, can only execute when simulator.run() is called.
+ *
+ */
+public class Start extends Event {
+
+	    public Start(double time) {
+	        super("Start", time);
+	    }
+	    @Override
+	    public void execute(Simulator sim) {
+	        super.execute(sim);
+	    }
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			
+		}
+		@Override
+		public String toString() {
+			// TODO Auto-generated method stub
+			return null;
 	}
 }
