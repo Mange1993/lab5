e0798dd3739a35d637b9193402d2855b78f624d2
Sara878 <sarnij-3@student.ltu.se>
2015-03-09 08:20:26 +0100

    Update Leave.java

--- src/lab5/carwash/Leave.java
+++ src/lab5/carwash/Leave.java
@@ -1,15 +1,41 @@
-package lab5.carwash;
+package carwash;
 
-import lab5.simulator.EventQueue;
+import lab5.simulator.*;
 
-public class Leave {
+public class Leave extends Event {
 	
 	EventQueue eventqueue;
 	CarWashState carState;
+        Car car;
+        
+        public Leave(double time, CarWashState carState, EventQueue eventqueue, Car car){
+            super.time = time;
+                this.carState = carState;
+		this.eventqueue = eventqueue;
+		this.car = car;  
+            
+            
+        }
 	
-	public void run(){	
-		Car Arrive =carState.getCar();
-		carState.setCar(Arrive);
+	public void run(){
+            
+            // Update the state data
+		//carState.setCurrentEvent(this);
+		//carState.setTime(time);
+		//carState.updateIdleTime();
+		//carState.updateQueueTime();
+		carState.setCar(car);
+            
+                 // changes view
+                        carState.setChanged();
+                        carState.notifyObservers();
+                        
+                        // if queue is not empty, schedule a new leave event
+                        // else, increase available washers
+                        
+                        
+            
+		
 	
 	
 		
