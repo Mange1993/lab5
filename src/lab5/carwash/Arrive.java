247974dd294b37669695e86de33f7faa72b36c96
Sara878 <sarnij-3@student.ltu.se>
2015-03-09 08:20:51 +0100

    Update Arrive.java

--- src/lab5/carwash/Arrive.java
+++ src/lab5/carwash/Arrive.java
@@ -1,5 +1,6 @@
-package lab5.carwash;
+package carwash;
 import lab5.simulator.*;
+
 /**
  * Class for the Event that a Car arrives
  * @author Sara
@@ -9,23 +10,73 @@ public class Arrive extends Event {
 	//CarWashState carState;
 	EventQueue eventqueue;
 	CarWashState carState;
-	private double time;
+	Event time;
 	
+        public Arrive(double _time, CarWashState carState, EventQueue eventqueue){
+            super._time =time;
+            this.carState = carState;
+            this.eventqueue =eventqueue;
+        }
+        
+        
+        
         /*
         Method that executes the Arrive Event
         */
-	public void run(){	
+	public void run(){
+            
+            
+            //A new car is made and arrives
 			Car Arrive =carState.getCar();
 			carState.setCar(Arrive);
-		
+                        
+                        // changes view
+                        carState.setChanged();
+                        carState.notifyObservers();
+                        
+                        //Checks whether the queue is full or if car can be added
 			if (carState.queueFull()){
 				carState.TotalRejected++;
 			} else{
 				carState.Carqueue.add(Arrive);
 				carState.totalcars++;
 			}
+                        
+                        
+                        //check to see if fast wash or slow wash is available
+                       if(carState.washers()){
+                        if(carState.emptyFastwash > 0 ){
+                            carState.emptyFastWash--;}
+                        else if(carState.emptySlowtwash >0 ){
+                            carState.emptyFSlowWash--;}
+                        
+        
+                        //car in machine leves queue
+                  Car cleaning = carState.Carqueue.first();
+                            carState.Carqueue.remove();
+                            
+                            
+                            //new leave event is scheduled, when wash is finished.
+                            Leave carLeave = new Leave();
+                            eventqueue.add(carLeave);
 	
 		}
+                       // New arrival
+                       	Arrive nextArrive = new Arrive(carState.getNextArrivalTime(),
+				carState, eventqueue);
+		eventqueue.addEvent(nextArrive);
+        
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
 	public String toString(){
 		if (carState.queueFull()){
 			return "Rejected";
