a51c9946c62363f5e6c7da79e43d71bd0bf47fd6
hammas95 <hammas95@outlook.com>
2015-03-09 08:45:35 +0100

    Update Simview.java

--- src/lab5/simulator/Simview.java
+++ src/lab5/simulator/Simview.java
@@ -1,15 +1,15 @@
 package lab5.simulator;
 
-import java.util.*;
 
-public abstract class Simview implements Observer{
-	
-	public Simview(SimState state){
-	
-	state.addObserver(this);
-	}
-	
-	public void update(Observable arg,  Object arg1 ){
- 
-}
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ * An abstraction of a View for the Simulator.
+ * The constructor adds the View as an observer to the state.
+ */
+public abstract class Simview implements Observer {
+    public Simview(SimState state) {
+        state.addObserver(this);
+    }
 }
