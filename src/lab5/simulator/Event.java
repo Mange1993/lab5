efd4b1f100b57fa815f52f8ba52984b545ad0b3f
hammas95 <hammas95@outlook.com>
2015-03-09 08:35:33 +0100

    Update Event.java

--- src/lab5/simulator/Event.java
+++ src/lab5/simulator/Event.java
@@ -6,20 +6,24 @@ package lab5.simulator;
 
 public abstract class Event {
 	
-	public abstract void run();
-		
-	public abstract String toString();
-	
-	protected String _type;
-	protected double _time;
+    protected String _type;
+    protected double _time;
     protected Simulator _simulator;
+    protected double _time;
+    
+    public double getTime(){
+    	return _time;
+    }
     
     public Event(String type, double time) {
         _type = type;
         _time = time;
     }
-
-	public void execute(Simulator sim) {
+    /**
+     * Executes the event, supposed to be overridden and called by super.
+     * @param sim The simulator executing the event.
+     */
+    public void execute(Simulator sim) {
         _simulator = sim;
     }
 }
